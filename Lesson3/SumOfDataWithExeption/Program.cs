5b523acc70dd1bdca8067e2098bbff7e8e2f4ffb
Jabeniy <64079697+Jabeniy@users.noreply.github.com>
2020-04-21 15:18:38 +0300

    Lesson3
    1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
    б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
    
    2. а) С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). Требуется подсчитать сумму всех нечетных положительных чисел. Сами числа и сумму вывести на экран, используя tryParse;
    б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные. При возникновении ошибки вывести сообщение. Напишите соответствующую функцию;
    
    3. *Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел. Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать программу, демонстрирующую все разработанные элементы класса. Достаточно решить 2 задачи. Все программы сделать в одном решении.
    ** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение
    ArgumentException("Знаменатель не может быть равен 0");
    Добавить упрощение дробей.

---
+++ Lesson3/SumOfDataWithExeption/Program.cs
@@ -0,0 +1,60 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDataWithExeption
+{
+    class Program
+    {
+        static int CheckExeptionByInput()
+        {
+            int result = 0;
+            bool exceptionCatched;
+            do
+            {
+                exceptionCatched = false;
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    exceptionCatched = true;
+                    string message = ex.Message;
+                    Console.WriteLine("Возникло исключение: " + message);
+                    Console.Write("Пожалуйста, повторите ввод: ");
+                }
+                
+            } while (exceptionCatched);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа подсчета нечетных положительных чисел");
+            Console.WriteLine("Вводите числа по одному, для завершения введите 0: ");
+
+            int AmountOfOddNumbers = 0;
+            while (true)
+            {
+                int number = CheckExeptionByInput();
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number > 0 && number % 2 == 1)
+                {
+                    AmountOfOddNumbers++;
+                }
+            }
+
+            Console.WriteLine(Environment.NewLine + "Количество чисел: " + AmountOfOddNumbers);
+
+            Console.ReadKey();
+
+        }
+    }
+}
